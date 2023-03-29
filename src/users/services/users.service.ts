0488f20991fef5549897f4fd5a187e5a460038ed
Ilyes-Ouni <59419657+Ilyes-Ouni@users.noreply.github.com>
2023-03-29 07:56:21 -0700

    Update users.service.ts

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from '../models/user.schema';
@@ -12,19 +12,42 @@ export class UsersService {
   }
 
   async findOne(id: string): Promise<User> {
-    return this.userModel.findById(id).exec();
+    try{
+      return this.userModel.findById(id).exec();
+    } catch (error) { 
+      return null
+    }
   }
 
-  async create(user: User): Promise<User> {
-    const createdUser = new this.userModel(user);
-    return createdUser.save();
+  async create(user: User): Promise<User> {    
+    try {
+      const existingUser = await this.userModel.findOne({email: user.email});
+      if(existingUser) return null
+
+      const createdUser = new this.userModel(user);
+      return await createdUser.save();
+    } catch (error) {
+        throw new HttpException('Server error', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   async update(id: string, user: User): Promise<User> {
-    return this.userModel.findByIdAndUpdate(id, user, { new: true }).exec();
+    try{
+        const existingUser = await this.userModel.findById(id).exec();
+        if (!existingUser) return null;
+        
+        let result = await existingUser.updateOne(user)
+        return result;
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   async delete(id: string): Promise<any> {
-    return this.userModel.findByIdAndDelete(id).exec();
+    try{
+      return await this.userModel.findByIdAndRemove(id).exec();
+    } catch (error) { 
+      return null
+    }
   }
 }
